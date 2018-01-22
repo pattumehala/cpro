8ff34b2e08549db0efa6ee3414058b0c74e21c56
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-01-22 16:17:26 +0530

    natural.C
    sum of natural numbers

--- natural.C
+++ natural.C
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
+{
 int n,i,sum=0;
 printf("Enter the value of n:");
 scanf("%d",&n);
 for(i=1;i<=n;i++)
 {
 sum=sum+i;
-printf("The sum is %d",sum);
 }
+printf("The sum is %d",sum);
 return 0;
 }
